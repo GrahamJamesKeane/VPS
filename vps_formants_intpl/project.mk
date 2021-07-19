8851d732728be35af3a84938fe161eed54b58bd0
Graham Keane <40239414+GrahamJamesKeane@users.noreply.github.com>
2021-07-19 02:32:29 +0100

    Add files via upload

---
+++ vps_formants_intpl/project.mk
@@ -0,0 +1,17 @@
+# #############################################################################
+# Project Customization
+# #############################################################################
+
+PROJECT = vps_formants_intpl
+
+UCSRC =
+
+UCXXSRC = vps_formants_intpl.cpp
+
+UINCDIR =
+
+UDEFS =
+
+ULIB = 
+
+ULIBDIR =
