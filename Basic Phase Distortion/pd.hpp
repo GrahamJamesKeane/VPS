73e74e9538c886b2a9d025d415a01e57b2531424
Graham Keane <40239414+GrahamJamesKeane@users.noreply.github.com>
2021-08-01 03:15:59 +0100

    Add files via upload

---
+++ Basic Phase Distortion/pd.hpp
@@ -0,0 +1,72 @@
+/*
+ * File: pd.hpp
+ *
+ * 2021 Graham Keane - Maynooth University
+ *
+ * Basic Phase Distortion
+ */
+ 
+#pragma once
+
+#include "userosc.h"
+
+#define ZEROF 0.f
+
+struct PD {
+	
+	enum {
+		flags_none = 0,
+		flag_reset = 1<<0
+	};
+	
+	struct Params {
+		float   shape;
+    
+		Params(void) :
+			shape(ZEROF)
+		{ }
+	};
+  
+	struct State {
+		float    phi; 		// Main oscillator phase
+		float    w0;		// Main oscillator pitch
+		uint32_t flags: 1;	// Bit field
+		State(void) :
+			phi(ZEROF),
+			w0(ZEROF),
+			flags(flags_none)
+		{
+			reset();
+		}
+		
+		inline void reset(void) {
+			phi = ZEROF;
+		}
+	};
+
+	PD(void) {
+		state = State();
+		params = Params();
+	}
+	
+	inline float PhaseDistortion(float phi) {
+		float d = params.shape;
+		
+		float pd;
+		if(phi < d) {
+			float fraction = phi / d;
+			pd = 0.5f * fraction;
+		} else {
+			float numerator = phi - d;
+			float denominator = 1.f - d;
+			float fraction = numerator / denominator;
+			pd = 0.5f * (1 + fraction);
+		}
+		
+		return -osc_cosf(pd);
+	}
+	
+	State 	state;
+	Params 	params;
+	
+};
